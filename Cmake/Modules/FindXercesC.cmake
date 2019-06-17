9a8ef1b2aea6195e4e964a684507b7396271694b
Josh.Chien <Josh.Chien@hotmail.com>
2019-06-17 13:06:29 +0800

    Update

--- Cmake/Modules/FindXercesC.cmake
+++ Cmake/Modules/FindXercesC.cmake
@@ -2,5 +2,5 @@
 
 set ( XercesC_FOUND True )
 set ( XercesC_INCLUDE_DIR 
-	"${CMAKE_SOURCE_DIR}/ThirdParty/Xerces-C/src/" 
-	"${CMAKE_BINARY_DIR}/ThirdParty/Xerces-C/src/")
+	"${CMAKE_SOURCE_DIR}/ThirdParty/XercesC/src/" 
+	"${CMAKE_BINARY_DIR}/ThirdParty/XercesC/src/")
